577f3cfe794841d8f2a4912c37fe1dbd2f71d2f2
Gabin Marignier <gabin.contact@gmail.com>
2020-03-20 11:03:11 +0100

    Add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
 import React from "react";
+import styled from "styled-components";
 
+import { colors } from "./ui";
 import { SearchPage } from "./SearchPage";
+import logo from "./Logo.svg";
 
-function App() {
+export default function App() {
   return (
-    <div>
+    <Root>
+      <Header>
+        <Logo src={logo} alt="Inato Anti-Covid Logo" />
+      </Header>
       <SearchPage />
-    </div>
+    </Root>
   );
 }
 
-export default App;
+const Root = styled.div`
+  background: ${colors.MainBackground};
+  height: 100%;
+`;
+
+const Header = styled.div`
+  display: flex;
+  align-items: center;
+  background: ${colors.SecondaryBackground};
+  height: 65px;
+  padding: 0 120px;
+`;
+
+const Logo = styled.img``;
---
+++ src/ui/colors.ts
@@ -0,0 +1,4 @@
+export enum colors {
+  MainBackground = "#F6F7FA",
+  SecondaryBackground = "#FFFFFF"
+}
