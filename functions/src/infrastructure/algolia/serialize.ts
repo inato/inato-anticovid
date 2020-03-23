465a7c06cd6b4f5f4244adb6b0fbc48e3a0e4030
Elliot <12149055+elliots-bits@users.noreply.github.com>
2020-03-23 14:14:15 +0100

    Updated cloud functions to handle new datamodel with direct JSONB arrays (#34)

--- functions/src/infrastructure/algolia/serialize.ts
+++ functions/src/infrastructure/algolia/serialize.ts
@@ -4,14 +4,6 @@ export const serialize = (trial: Trial): Object => {
   return {
     objectID: trial.trialid,
     ...trial.rest,
-    countries: (trial.rest.countries ?? "").split(";"),
-    therapeutic_classes: (trial.rest.therapeutic_classes ?? "").split(";"),
-    surrogate_outcome_extracted_: (
-      trial.rest.surrogate_outcome_extracted_ ?? ""
-    ).split(";"),
-    clinical_outcome_extracted_: (
-      trial.rest.clinical_outcome_extracted_ ?? ""
-    ).split(";"),
     exclusion_criteria: (trial.rest.exclusion_criteria ?? "").slice(0, 500),
     inclusion_criteria: (trial.rest.inclusion_criteria ?? "").slice(0, 500)
   };
