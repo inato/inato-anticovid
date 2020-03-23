07e6a8bd3d44b3bec422dc87a50f9c9dcce6a306
Elliot <12149055+elliots-bits@users.noreply.github.com>
2020-03-23 14:14:15 +0100

    Updated cloud functions to handle new datamodel with direct JSONB arrays (#34)

--- functions/src/infrastructure/algolia/serialize.test.ts
+++ functions/src/infrastructure/algolia/serialize.test.ts
@@ -7,13 +7,9 @@ describe("serialize", () => {
       trialid: "trialid"
     });
     expect(serialize(trial)).toStrictEqual({
-      clinical_outcome_extracted_: [""],
-      countries: [""],
       exclusion_criteria: "",
       inclusion_criteria: "",
       objectID: "trialid",
-      surrogate_outcome_extracted_: [""],
-      therapeutic_classes: [""]
     });
   });
 
@@ -25,14 +21,10 @@ describe("serialize", () => {
       }
     });
     expect(serialize(trial)).toStrictEqual({
-      clinical_outcome_extracted_: [""],
-      countries: [""],
       exclusion_criteria: "",
       inclusion_criteria: "",
       objectID: "trialid",
       other: "other",
-      surrogate_outcome_extracted_: [""],
-      therapeutic_classes: [""]
     });
   });
 });
