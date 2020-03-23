e2a2f0d011fc7b8e3a37a2d3e792456e6ee4f81a
Gabin Marignier <gabin.contact@gmail.com>
2020-03-23 18:22:53 +0100

    Add non-empty page

---
+++ src/OverviewPage/OverviewPage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styled from "styled-components";
+
+export const OverviewPage = () => <Container />;
+
+const Container = styled.div`
+  height: 100vh;
+  padding: 32px 120px;
+`;
