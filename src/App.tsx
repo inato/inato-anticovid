e87e0942ae2d19757c3b35dc4852450e3c5e9950
Julien Fouilhé <julien.fouilhe@gmail.com>
2020-03-23 15:53:20 +0100

    Add OverviewPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
 import React from "react";
 import styled from "styled-components";
-import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Redirect, Link } from "react-router-dom";
 
 import { colors } from "./ui";
 import { SearchPage } from "./SearchPage";
 import logo from "./Logo.svg";
+import { OverviewPage } from "./OverviewPage";
 
 export default function App() {
   return (
     <Root>
-      <Header>
-        <Logo src={logo} alt="Inato Anti-Covid Logo" />
-      </Header>
-      <Separator />
       <BrowserRouter>
+        <Header>
+          <Logo src={logo} alt="Inato Anti-Covid Logo" />
+          <HeaderLink to="/overview">Explore</HeaderLink>
+          <HeaderLink to="/search">Search trials</HeaderLink>
+        </Header>
+        <Separator />
         <Switch>
           <Route exact path="/">
             {() => <Redirect to="/search" />}
           </Route>
-          <Route path="/search">
+          <Route exact path="/overview">
+            <OverviewPage />
+          </Route>
+          <Route exact path="/search">
             <SearchPage />
           </Route>
         </Switch>
@@ -40,9 +46,24 @@ const Header = styled.div`
   padding: 0 120px;
 `;
 
-const Logo = styled.img``;
+const Logo = styled.img`
+  margin-right: 48px;
+`;
 
 const Separator = styled.div`
   background: ${colors.Separator};
   height: 1px;
+`;
+
+const HeaderLink = styled(({ active, ...props }) => <Link {...props} />)`
+  font-size: 16px;
+  line-height: 20px;
+  text-transform: uppercase;
+  color: #647b91;
+  text-decoration: none;
+  margin-right: 33px;
+
+  &:last-child {
+    margin-right: 0;
+  }
 `;
