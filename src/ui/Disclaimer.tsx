3477c385bafcd29b2e90589eeaa6727e49fbbdb3
Jeremie Dayan <dayanjeremie@gmail.com>
2020-03-24 18:02:32 +0100

    disclaimer

--- src/ui/Disclaimer.tsx
+++ src/ui/Disclaimer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 
-import { colors } from "../ui";
+import { colors } from ".";
 
 export const Disclaimer = () => (
   <Container>
