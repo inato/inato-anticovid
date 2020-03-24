258a49686ffeb394391d8cd352b3f0431dd93956
Laure Retru-Chavastel <laure.retru-chavastel@gadz.org>
2020-03-24 16:05:55 +0100

    :sparkles: (Accordion) Add icon + transition effect

---
+++ src/ui/ArrowDownIcon.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export const ArrowDownIcon = ({ className }: { className?: string }) => {
+  return (
+    <svg
+      className={className}
+      width="11"
+      height="10"
+      viewBox="0 0 11 10"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M9.6875 3.04614L5.22083 7.51239C5.16229 7.57102 5.08285 7.60396 5 7.60396C4.91715 7.60396 4.83771 7.57102 4.77917 7.51239L0.3125 3.04614"
+        stroke="#124469"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
