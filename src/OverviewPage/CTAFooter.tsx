2481b790438f45ccb6bd40cd92ac9ce55bb7849b
Gabin Marignier <gabin.contact@gmail.com>
2020-03-24 11:58:49 +0100

    Finish form

--- src/OverviewPage/CTAFooter.tsx
+++ src/OverviewPage/CTAFooter.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import { NavLink } from "react-router-dom";
 
 import { colors, devices } from "../ui";
 
@@ -12,34 +13,48 @@ export const CTAFooter = ({ className }: { className?: string }) => (
       <SecondaryParagraph>
         No spam, at most twice a week, unsubscribe anytime
       </SecondaryParagraph>
-      <form
-        method="POST"
-        action="#"
-        onSubmit={async event => {
-          event.preventDefault();
-
-          const target = event.target as HTMLFormElement;
-          const body = {
-            email:
-              (target.elements.namedItem("email") as HTMLInputElement).value ??
-              ""
-          };
-          await fetch(ZappierHookUrl, {
-            method: "POST",
-            body: JSON.stringify(body)
-          });
-        }}
-      >
-        <Input type="email" name="email" placeholder="Your email..." />
-        <button type="submit">Suscribe</button>
-      </form>
+      <Form />
     </LeftCard>
     <RightCard>
       <Title>Want to go further?</Title>
-      <Paragraph>Make your own analysis by searching trials.</Paragraph>
+      <Paragraph>
+        Make your own analysis by{" "}
+        <NavLink to="/search">searching trials</NavLink>.
+      </Paragraph>
     </RightCard>
   </Container>
 );
+
+const Form = () => {
+  const [isFormSent, setFormSent] = useState(false);
+  if (isFormSent) {
+    return <Paragraph>You have been successfully suscribed!</Paragraph>;
+  }
+
+  return (
+    <form
+      method="POST"
+      action="#"
+      onSubmit={async event => {
+        event.preventDefault();
+
+        const target = event.target as HTMLFormElement;
+        const body = {
+          email:
+            (target.elements.namedItem("email") as HTMLInputElement).value ?? ""
+        };
+        await fetch(ZappierHookUrl, {
+          method: "POST",
+          body: JSON.stringify(body)
+        });
+        setFormSent(true);
+      }}
+    >
+      <Input type="email" name="email" placeholder="Your email..." />
+      <Button type="submit">Suscribe</Button>
+    </form>
+  );
+};
 
 const Container = styled.div`
   display: flex;
@@ -51,11 +66,13 @@ const Container = styled.div`
 `;
 
 const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: auto;
   background-color: ${colors.SecondaryBackground};
   border-radius: 4px;
   padding: 16px;
   margin-bottom: 16px;
-  width: 100%;
 `;
 
 const LeftCard = styled(Card)``;
@@ -90,4 +107,12 @@ const Input = styled.input`
   border-radius: 4px;
   border: 1px solid ${colors.Border};
   padding: 8px 16px;
+  margin-right: 8px;
+`;
+
+const Button = styled.button`
+  border-radius: 4px;
+  padding: 8px 16px;
+  color: ${colors.SecondaryBackground};
+  background-color: ${colors.Primary};
 `;
