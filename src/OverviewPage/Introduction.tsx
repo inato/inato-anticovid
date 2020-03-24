6ee76d8bb2e968842f6856484cc15680bd4da387
Bastien Duret <bastien.duret@gmail.com>
2020-03-24 11:12:14 +0100

    ✨ Add introduction block to analysis page

---
+++ src/OverviewPage/Introduction.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import styled from "styled-components";
+
+import { colors, fontSize, fontWeight, lineHeight } from "../ui";
+
+export const Introduction = () => (
+  <Container>
+    <Title>What is Anticovid?</Title>
+    <Paragraph>
+      Anticovid is an open access platform gathering virtually all available
+      information in a transparent manner regarding clinical research worldwide
+      against SARS-CoV 2 (which is the virus responsible for COVID-19)
+    </Paragraph>
+    <Paragraph>
+      The purpose of Anticovid is to help the global healthcare community to get
+      access to the needed information in a comprehensive way, especially given
+      that the landscape is evolving at an unprecedented pace. This is critical
+      to optimize resource allocation and address unmet needs as fast as
+      possible.
+    </Paragraph>
+  </Container>
+);
+
+const Container = styled.div`
+  background: ${colors.SecondaryBackground};
+  color: ${colors.DefaultText};
+  display: flex;
+  flex-direction: column;
+  padding: 16px 32px;
+`;
+
+const Title = styled.h1`
+  font-size: ${fontSize.Big};
+  font-weight: ${fontWeight.Bold};
+  line-height: ${lineHeight.Big};
+  margin: 0 0 4px 0;
+`;
+
+const Paragraph = styled.p`
+  margin: 4px 0 0 0;
+`;
--- src/OverviewPage/OverviewPage.tsx
+++ src/OverviewPage/OverviewPage.tsx
@@ -5,12 +5,14 @@ import TableauReport from "tableau-react";
 import { device } from "../ui";
 
 import { Disclaimer } from "./Disclaimer";
+import { Introduction } from "./Introduction";
 
 const TableauURL =
   "https://public.tableau.com/views/Who_15849588323430/ClinicalResearchforCOVID-19";
 
 export const OverviewPage = () => (
   <Container>
+    <Introduction />
     <TableauReport url={TableauURL} />
     <Disclaimer />
   </Container>
@@ -18,6 +20,13 @@ export const OverviewPage = () => (
 
 const Container = styled.div`
   padding: 32px;
+
+  > div:not(:last-child) {
+    margin-bottom: 16px;
+  }
+  > div {
+    border-radius: 4px;
+  }
 
   @media ${device.sm} {
     padding: 32px 120px;
