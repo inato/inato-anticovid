d45c55c02704932ea1213bf99eff17cdb554ee8d
Gabin Marignier <gabin.contact@gmail.com>
2020-03-24 11:37:00 +0100

    Setup zap hook

--- src/OverviewPage/CTAFooter.tsx
+++ src/OverviewPage/CTAFooter.tsx
@@ -1,11 +1,39 @@
 import React from "react";
-import styled, { StyledComponent } from "styled-components";
+import styled from "styled-components";
 
 import { colors, devices } from "../ui";
 
+const ZappierHookUrl = "https://hooks.zapier.com/hooks/catch/6538069/o1x0ecd/";
+
 export const CTAFooter = ({ className }: { className?: string }) => (
   <Container className={className}>
-    <LeftCard />
+    <LeftCard>
+      <Title>Stay updated with new analyses</Title>
+      <SecondaryParagraph>
+        No spam, at most twice a week, unsubscribe anytime
+      </SecondaryParagraph>
+      <form
+        method="POST"
+        action="#"
+        onSubmit={async event => {
+          event.preventDefault();
+
+          const target = event.target as HTMLFormElement;
+          const body = {
+            email:
+              (target.elements.namedItem("email") as HTMLInputElement).value ??
+              ""
+          };
+          await fetch(ZappierHookUrl, {
+            method: "POST",
+            body: JSON.stringify(body)
+          });
+        }}
+      >
+        <Input type="email" name="email" placeholder="Your email..." />
+        <button type="submit">Suscribe</button>
+      </form>
+    </LeftCard>
     <RightCard>
       <Title>Want to go further?</Title>
       <Paragraph>Make your own analysis by searching trials.</Paragraph>
@@ -27,12 +55,7 @@ const Card = styled.div`
   border-radius: 4px;
   padding: 16px;
   margin-bottom: 16px;
-
-  display: flex;
-  flex: 1;
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: center;
+  width: 100%;
 `;
 
 const LeftCard = styled(Card)``;
@@ -54,4 +77,17 @@ const Paragraph = styled.p`
   font-size: 14px;
   font-weight: normal;
   margin: 0;
+`;
+
+const SecondaryParagraph = styled.p`
+  color: ${colors.GreySecondaryText};
+  font-size: 12px;
+  font-weight: normal;
+  margin: 0;
+`;
+
+const Input = styled.input`
+  border-radius: 4px;
+  border: 1px solid ${colors.Border};
+  padding: 8px 16px;
 `;
