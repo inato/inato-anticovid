ac0155785456ccab7de25b69ec0c279173921d61
Laure Retru-Chavastel <laure.retru-chavastel@gadz.org>
2020-03-24 17:48:02 +0100

    :recycle: Use right icons for arrows

--- src/ui/ArrowDownIcon.tsx
+++ src/ui/ArrowDownIcon.tsx
@@ -4,15 +4,15 @@ export const ArrowDownIcon = ({ className }: { className?: string }) => {
   return (
     <svg
       className={className}
-      width="11"
-      height="10"
-      viewBox="0 0 11 10"
+      width="12"
+      height="12"
+      viewBox="0 0 12 12"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       stroke="#124469"
     >
       <path
-        d="M9.6875 3.04614L5.22083 7.51239C5.16229 7.57102 5.08285 7.60396 5 7.60396C4.91715 7.60396 4.83771 7.57102 4.77917 7.51239L0.3125 3.04614"
+        d="M10.6875 4.04614L6.22083 8.51239C6.16229 8.57102 6.08285 8.60396 6 8.60396C5.91715 8.60396 5.83771 8.57102 5.77917 8.51239L1.3125 4.04614"
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
