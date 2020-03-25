6265820e85d8772b305c8e021419c0e0bf48e117
Vincent François <vincent.francois@inato.com>
2020-03-25 12:39:16 +0100

    39 follow up (#91)
    * Move the useBoolean to specific file
    
    * Replace the filteringContext by simple props
    
    * Move the FilterTrialsButton in a specific file

---
+++ src/hooks/useBolean.ts
@@ -0,0 +1,19 @@
+import { useState, useCallback, useMemo } from "react";
+
+export const useBoolean = (initialValue: boolean) => {
+  const [isTrue, setState] = useState(() => initialValue);
+  const setToTrue = useCallback(() => setState(() => true), []);
+  const setToFalse = useCallback(() => setState(() => false), []);
+  const toggle = useCallback(() => setState(prev => !prev), []);
+
+  return useMemo(
+    () => ({
+      isTrue,
+      isFalse: !isTrue,
+      setToTrue,
+      setToFalse,
+      toggle
+    }),
+    [isTrue, setToFalse, setToTrue, toggle]
+  );
+};
