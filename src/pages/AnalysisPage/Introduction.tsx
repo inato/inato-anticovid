91882651fad4b264bb9807afbb497410aac574b8
vinassefranche <vincent.francois@inato.com>
2020-03-25 17:48:21 +0100

    Add the laboratory icon to the analysis page

--- src/pages/AnalysisPage/Introduction.tsx
+++ src/pages/AnalysisPage/Introduction.tsx
@@ -4,54 +4,66 @@ import styled from "styled-components";
 import { colors, fontSize, fontWeight, lineHeight, devices } from "../../ui";
 
 import { Accordion } from "./Accordion";
+import icon from "./laboratory.svg";
+import { LaboratoryIcon } from "./LaboratoryIcon";
 
 export const Introduction = () => (
   <Container>
-    <Title>What is Anticovid?</Title>
-    <Paragraph>
-      Anticovid is an open-access platform gathering all available information
-      on clinical research worldwide against SARS-CoV 2 (the virus responsible
-      for COVID-19)
-    </Paragraph>
-    <Paragraph>
-      The purpose of Anticovid is to provide access to helpful information to
-      the global healthcare community, in a constantly evolving landscape. We
-      believe this is critical to optimize resource allocation and address unmet
-      needs as fast as possible
-    </Paragraph>
-    <Accordion title="Where does the data come from">
+    <TextContainer>
+      <Title>What is Anticovid?</Title>
       <Paragraph>
-        Most of the data comes from public sources that are duly cited in the
-        dedicated section of the website. Data is updated every day.
+        Anticovid is an open access platform gathering virtually all available
+        information in a transparent manner regarding clinical research
+        worldwide against SARS-CoV 2 (which is the virus responsible for
+        COVID-19).
       </Paragraph>
       <Paragraph>
-        Anticovid will soon enhance its content with high quality data in
-        particular from clinical investigators around the globe that are
-        committed to support research against SARS-CoV 2, as well as
-        pharmaceutical manufacturers already engaged or about to do so in
-        vaccine or therapeutic trials.
+        The purpose of Anticovid is to provide access to helpful information to
+        the global healthcare community, in a constantly evolving landscape. We
+        believe this is critical to optimize resource allocation and address
+        unmet needs as fast as possible
       </Paragraph>
-    </Accordion>
-    <Accordion title="Who are we">
-      <Paragraph>
-        Anticovid is provided by{" "}
-        <a href="https://inato.com/" target="_blank" rel="noopener noreferrer">
-          Inato
-        </a>
-        .
-      </Paragraph>
-      <Paragraph>
-        Inato is a platform that increases the pool of available patients for
-        trials by:
-        <ul>
-          <li>Matching the best sites for any given study</li>
-          <li>
-            Ensuring that site partners in our marketplace are successfully
-            delivering trials.
-          </li>
-        </ul>
-      </Paragraph>
-    </Accordion>
+      <Accordion title="Where does the data come from">
+        <Paragraph>
+          Most of the data comes from public sources that are duly cited in the
+          dedicated section of the website. Data is updated every day.
+        </Paragraph>
+        <Paragraph>
+          Anticovid will soon enhance its content with high quality data in
+          particular from clinical investigators around the globe that are
+          committed to support research against SARS-CoV 2, as well as
+          pharmaceutical manufacturers already engaged or about to do so in
+          vaccine or therapeutic trials.
+        </Paragraph>
+      </Accordion>
+      <Accordion title="Who are we">
+        <Paragraph>
+          Anticovid is provided by{" "}
+          <a
+            href="https://inato.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Inato
+          </a>
+          .
+        </Paragraph>
+        <Paragraph>
+          Inato is a platform that increases the pool of available patients for
+          trials by:
+          <ul>
+            <li>Matching the best sites for any given study</li>
+            <li>
+              Ensuring that site partners in our marketplace are successfully
+              delivering trials.
+            </li>
+          </ul>
+        </Paragraph>
+      </Accordion>
+    </TextContainer>
+    <IconContainer>
+      <StyledLaboratoryIcon />
+    </IconContainer>
   </Container>
 );
 
@@ -60,16 +72,24 @@ const Container = styled.div`
   color: ${colors.DefaultText};
   display: flex;
   flex-direction: column;
-  padding: 16px;
+  padding: 16px 16px 0 16px;
+
   @media ${devices.Desktop} {
-    border-left: 8px solid ${colors.GreySecondaryText};
-    padding: 16px 32px;
+    flex-direction: row;
+    padding: 16px 32px 0 32px;
   }
   & ${Accordion} {
     margin-top: 8px;
     :first-of-type {
       margin-top: 16px;
     }
+  }
+`;
+
+const TextContainer = styled.div`
+  @media ${devices.Desktop} {
+    padding-bottom: 16px;
+    margin-right: 32px;
   }
 `;
 
@@ -86,4 +106,19 @@ const Title = styled.h1`
 
 const Paragraph = styled.p`
   margin: 4px 0 0 0;
+`;
+
+const IconContainer = styled.div`
+  display: flex;
+  align-self: center;
+  @media ${devices.Desktop} {
+    align-self: flex-end;
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const StyledLaboratoryIcon = styled(LaboratoryIcon)`
+  width: 100%;
+  height: 100%;
 `;
