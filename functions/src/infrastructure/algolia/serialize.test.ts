28327c41a755e2fe4bbf7726b06eafc2c778c355
Scott Picquerey <scott.picquerey@inato.com>
2020-03-25 14:45:20 +0100

    ♻️ Use camelcase instead of snake_case for Trial props

--- functions/src/infrastructure/algolia/serialize.test.ts
+++ functions/src/infrastructure/algolia/serialize.test.ts
@@ -5,12 +5,12 @@ describe("serialize", () => {
   it("should serialize all fields", () => {
     const date = new Date();
     const trial = trialFactory({
-      trialid: "trialid",
-      public_title: "public_title",
-      web_address: "web_address",
-      recruitment_status: "recruitment_status",
-      therapeutic_classes: ["therapeutic_classes"],
-      date_registration3: date
+      trialId: "trialid",
+      publicTitle: "public_title",
+      webAddress: "web_address",
+      recruitmentStatus: "recruitment_status",
+      therapeuticClasses: ["therapeutic_classes"],
+      dateRegistration3: date
     });
     expect(serialize(trial)).toStrictEqual({
       exclusion_criteria: "",
@@ -26,7 +26,7 @@ describe("serialize", () => {
 
   it("should serialize a null recruitment status", () => {
     const trial = trialFactory({
-      recruitment_status: null
+      recruitmentStatus: null
     });
     expect(serialize(trial)).toMatchObject({
       recruitment_status: null
@@ -35,7 +35,7 @@ describe("serialize", () => {
 
   it("should serialize a trial with additional parameters", () => {
     const trial = trialFactory({
-      trialid: "trialid",
+      trialId: "trialid",
       rest: {
         other: "other"
       }
