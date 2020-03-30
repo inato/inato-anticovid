8824798fe48d302d4f2051cf9f4cd51e38c48557
Julien Fouilhé <julien.fouilhe@gmail.com>
2020-03-30 17:45:46 +0200

    Add the presentation layer

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,47 +1,17 @@
 import * as functions from "firebase-functions";
 
 import {
-  setupPostgresClient,
-  PostgresTrialRepository,
-  AlgoliaIndexingService,
-  setupAlgoliaIndex
-} from "./infrastructure";
-import { refreshTrialIndex, setIndexSettings } from "./application";
+  setAlgoliaSettingsHandler,
+  uploadToAlgoliaHandler
+} from "./presentation";
 
 export const uploadToAlgolia = functions
   .runWith({
     timeoutSeconds: 500,
     memory: "1GB"
   })
-  .https.onRequest(async (_request, response) => {
-    const client = await setupPostgresClient();
-    const tableName = functions.config().pg.tablename;
-    const trialRepository = new PostgresTrialRepository(client, tableName);
-
-    const algoliaIndex = setupAlgoliaIndex({
-      apiKey: functions.config().algolia.apikey,
-      indexName: functions.config().algolia.index
-    });
-    const indexingService = new AlgoliaIndexingService(algoliaIndex);
-
-    const trialsCount = await refreshTrialIndex({
-      indexingService,
-      trialRepository
-    });
-    await client.end();
-    response.send(`Indexed ${trialsCount} trials`);
-  });
+  .https.onRequest(uploadToAlgoliaHandler);
 
 export const setAlgoliaSettings = functions.https.onRequest(
-  async (_request, response) => {
-    const algoliaIndex = setupAlgoliaIndex({
-      apiKey: functions.config().algolia.apikey,
-      indexName: functions.config().algolia.index
-    });
-    const indexingService = new AlgoliaIndexingService(algoliaIndex);
-
-    await setIndexSettings({ indexingService });
-
-    response.send(`Algolia settings have been set`);
-  }
+  setAlgoliaSettingsHandler
 );
