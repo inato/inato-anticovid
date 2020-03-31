ff186d033929bdbf2138b75e8e13e9621b8a64b4
ThierryCols <thierrycolsenet@gmail.com>
2020-03-31 18:06:35 +0200

    ✨ add searchTrials to the indexing service

--- functions/src/infrastructure/algolia/AlgoliaIndexingService.ts
+++ functions/src/infrastructure/algolia/AlgoliaIndexingService.ts
@@ -1,10 +1,12 @@
-import { IndexingService } from "../../application";
+import { IndexingService, FacetFilters } from "../../application";
 import { Trial } from "../../domain";
 import { serialize } from "./serialize";
 import { SearchIndex } from "algoliasearch";
 import * as TaskEither from "fp-ts/lib/TaskEither";
 import { unknownError } from "../../domain/errors";
 import { pipe } from "fp-ts/lib/pipeable";
+import { deserializeSearchTrialsResponse } from "./deserialize";
+import { Option, getOrElse } from "fp-ts/lib/Option";
 
 export class AlgoliaIndexingService implements IndexingService {
   constructor(private readonly algoliaIndex: SearchIndex) {}
@@ -58,6 +60,61 @@ export class AlgoliaIndexingService implements IndexingService {
           )
       ),
       TaskEither.map(() => undefined)
+    );
+  }
+
+  stringifyFacetFilters({
+    recruitment_status = [],
+    therapeutic_classes = [],
+    clinical_outcome_extracted_ = [],
+    surrogate_outcome_extracted_ = [],
+    study_type = [],
+    countries = []
+  }: Partial<FacetFilters>) {
+    return [
+      recruitment_status.map(
+        recruitmentStatus => `recruitment_status:${recruitmentStatus}`
+      ),
+      therapeutic_classes.map(
+        therapeuticClass => `therapeutic_classes:${therapeuticClass}`
+      ),
+      clinical_outcome_extracted_.map(
+        clinicalOutcomeExtracted =>
+          `clinical_outcome_extracted_:${clinicalOutcomeExtracted}`
+      ),
+      surrogate_outcome_extracted_.map(
+        surrogateOutcomeExtracted =>
+          `surrogate_outcome_extracted_:${surrogateOutcomeExtracted}`
+      ),
+      study_type.map(studyType => `study_type:${studyType}`),
+      countries.map(country => `countries:${country}`)
+    ];
+  }
+
+  searchTrials({
+    searchQuery,
+    facetFilters
+  }: {
+    searchQuery: Option<string>;
+    facetFilters: Partial<FacetFilters>;
+  }) {
+    const facetFiltersToSearch = this.stringifyFacetFilters(facetFilters);
+    return pipe(
+      TaskEither.tryCatch(
+        () =>
+          (async () =>
+            this.algoliaIndex.search(getOrElse(() => "")(searchQuery), {
+              facetFiltersToSearch,
+              attributesToRetrieve: ["objectID"]
+            }))(),
+        e =>
+          unknownError(
+            e instanceof Error
+              ? e.message
+              : "Unknown algolia replacing all objects error"
+          )
+      ),
+      TaskEither.map(response => deserializeSearchTrialsResponse(response.hits))
     );
   }
 }
