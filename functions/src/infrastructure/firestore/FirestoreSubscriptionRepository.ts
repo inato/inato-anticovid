139f19f453fffdac5ae4bb2185067b0e138fe3c5
Julien Fouilhé <julien.fouilhe@gmail.com>
2020-04-01 00:22:45 +0200

    Add tests for FirestoreSubscriptionRepository

--- functions/src/infrastructure/firestore/FirestoreSubscriptionRepository.ts
+++ functions/src/infrastructure/firestore/FirestoreSubscriptionRepository.ts
@@ -1,11 +1,13 @@
 import * as admin from "firebase-admin";
 import * as TaskEither from "fp-ts/lib/TaskEither";
+import { array } from "fp-ts/lib/Array";
 
 import { SubscriptionRepository, Subscription } from "../../domain";
-import { unknownError } from "../../domain/errors";
+import { unknownError, invalidInformationError } from "../../domain/errors";
 import { pipe } from "fp-ts/lib/pipeable";
 import { deserialize } from "./deserialize";
 import { serialize } from "./serialize";
+import { taskEitherExtend } from "../../domain/utils/taskEither";
 
 const SUBSCRIPTION_COLLECTION_NAME = "subscriptions";
 
@@ -29,8 +31,18 @@ export class FirestoreSubscriptionRepository implements SubscriptionRepository {
             e instanceof Error ? e.message : "Unknown firestore get error"
           )
       ),
-      TaskEither.map(results =>
-        results.docs.map(document => deserialize(document.data()))
+      taskEitherExtend(results =>
+        array.traverse(TaskEither.taskEither)(results.docs, document =>
+          TaskEither.tryCatch(
+            async () => deserialize(document.data()),
+            e =>
+              invalidInformationError(
+                e instanceof Error
+                  ? e.message
+                  : "Invalid document from firebase"
+              )
+          )
+        )
       )
     );
   }
