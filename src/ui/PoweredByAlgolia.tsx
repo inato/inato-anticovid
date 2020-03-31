b81f87037743d2e0f5d474c9ecc0c738ef77c261
Laure Retru-Chavastel <laure.retru-chavastel@gadz.org>
2020-03-31 14:30:54 +0200

    :sparkles: Create new custom component PoweredByAlgolia

---
+++ src/ui/PoweredByAlgolia.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import styled from "styled-components";
+
+import { colors } from "./colors";
+import { AlgoliaIcon } from "./icons/AlgoliaLogo";
+
+const StyledAlgoliaIcon = styled(AlgoliaIcon)`
+  height: 16px;
+  width: 57px;
+  margin-left: 5px;
+`;
+
+const PoweredByContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const PoweredByText = styled.span`
+  color: ${colors.GreySecondaryText};
+  font-size: 12px;
+`;
+const PoweredByLink = styled.a`
+  display: flex;
+`;
+
+export const PoweredByAlgolia = ({ className }: { className?: string }) => (
+  <PoweredByContainer className={`ais-PoweredBy ${className}`}>
+    <PoweredByText className="ais-PoweredBy-text">
+      Search powered by
+    </PoweredByText>
+    <PoweredByLink
+      href="https://www.algolia.com"
+      className="ais-PoweredBy-link"
+      aria-label="Algolia"
+    >
+      <StyledAlgoliaIcon />
+    </PoweredByLink>
+  </PoweredByContainer>
+);
