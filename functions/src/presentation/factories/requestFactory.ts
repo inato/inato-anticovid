4a7c2098ae42fd385f9414c239482a97f2b2eb45
Julien Fouilhé <julien.fouilhe@gmail.com>
2020-04-01 14:07:53 +0200

    Add test for handler

--- functions/src/presentation/factories/requestFactory.ts
+++ functions/src/presentation/factories/requestFactory.ts
@@ -1,4 +1,6 @@
 import * as functions from "firebase-functions";
 
-export const requestFactory = ({}: Partial<{}> = {}) =>
-  ({} as functions.https.Request);
+export const requestFactory = ({
+  query = {}
+}: Partial<{ query: { [key: string]: unknown } }> = {}) =>
+  ({ query } as functions.https.Request);
