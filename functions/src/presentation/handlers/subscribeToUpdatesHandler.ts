8d826816031fa5dce20f14a8a4adce883b6188b6
ThierryCols <thierrycolsenet@gmail.com>
2020-04-01 10:41:52 +0200

    :wip: wip :sad:

--- functions/src/presentation/handlers/subscribeToUpdatesHandler.ts
+++ functions/src/presentation/handlers/subscribeToUpdatesHandler.ts
@@ -2,22 +2,30 @@ import * as functions from "firebase-functions";
 import { pipe } from "fp-ts/lib/pipeable";
 import * as TaskEither from "fp-ts/lib/TaskEither";
 import * as Task from "fp-ts/lib/Task";
+import * as decod from "decod";
 import {
   SubscriptionRepository,
   Subscription,
   EmailAddress
 } from "../../domain";
 import { taskEitherExtend } from "../../domain/utils/taskEither";
+import { IndexingService } from "../../application";
+import { fromNullable } from "fp-ts/lib/Option";
 
 export const subscribeToUpdatesHandler = ({
-  subscriptionRepository
+  subscriptionRepository,
+  indexingService
 }: {
   subscriptionRepository: SubscriptionRepository;
+  indexingService: IndexingService;
 }) => async (
-  _request: functions.https.Request,
+  request: functions.https.Request,
   response: functions.Response<any>
 ) =>
   pipe(
+    request.body,
+    getFacetFiltersQueryFromRequest,
+    indexingService.searchTrials,
     subscriptionRepository.store(
       Subscription.build({
         email: EmailAddress.unsafe_parse("julien@inato.com"),
@@ -41,3 +49,11 @@ export const subscribeToUpdatesHandler = ({
       }
     )
   )();
+
+const getFacetFiltersQueryFromRequest = (request: functions.https.Request) =>
+  decod.props({
+    searchQuery: fromNullable(
+      decod.at("search_query", decod.nullable(decod.string))
+    ),
+    facetFilters: decod.at("facet_filters", decod.array(decod.string))
+  })(request.body);
