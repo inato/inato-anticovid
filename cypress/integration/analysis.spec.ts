44290d64f44b9d03a86a32f9021d62eb35149f53
jrmdayn <dayanjeremie@gmail.com>
2020-04-01 09:08:57 +0200

    final touch

--- cypress/integration/analysis.spec.ts
+++ cypress/integration/analysis.spec.ts
@@ -12,10 +12,15 @@ describe("Analysis page", () => {
   devices.forEach(deviceType => {
     describe(deviceType, () => {
       beforeEach(() => {
+        // prevent window.fetch, fallback on xhr for cypress compat
         cy.on("window:before:load", win => {
           delete win.fetch;
         });
+
+        // select the device
         cy.device(deviceType);
+
+        // go to root page
         cy.visit("/");
       });
 
@@ -24,35 +29,41 @@ describe("Analysis page", () => {
         cy.location("pathname").should("eq", "/analysis");
 
         // should have a header
-        getHeader().within(() => {
-          // should have the logo top left
-          cy.get(`[alt="Inato Anti-Covid Logo"]`);
+        getHeader()
+          .should("be.visible")
+          .within(() => {
+            // should have the logo top left
+            cy.get(`[alt="Inato Anti-Covid Logo"]`);
 
-          // Should have links to analysis and search trial pages
-          cy.contains("a", "Analysis").should("have.class", "active");
-          cy.contains("a", "Search trials");
+            // Should have links to analysis and search trial pages
+            cy.contains("a", "Analysis").should("have.class", "active");
+            cy.contains("a", "Search trials");
 
-          // Should have send us feedback link
-          cy.contains("a", "Send us feedback").then($elt => {
-            // is visible only on large device
-            const isVisible = deviceType === "large";
-            cy.wrap($elt.is(":visible")).should("eq", isVisible);
+            // Should have send us feedback link
+            cy.contains("a", "Send us feedback").then($elt => {
+              // is visible only on large device
+              const isVisible = deviceType === "large";
+              cy.wrap($elt.is(":visible")).should("eq", isVisible);
+            });
           });
-        });
 
         // Should have intro section
-        getIntro().within(() => {
-          cy.contains("What is anticovid?");
-          cy.contains("Where the data comes from").click();
-          cy.contains("Most of the data comes");
-          cy.contains("Who we are").click();
-          cy.contains("Anticovid is provided by Inato");
-        });
+        getIntro()
+          .should("be.visible")
+          .within(() => {
+            cy.contains("What is anticovid?");
+            cy.contains("Where the data comes from").click();
+            cy.contains("Most of the data comes");
+            cy.contains("Who we are").click();
+            cy.contains("Anticovid is provided by Inato");
+          });
 
         // Should have tableau section
-        getTableau().within(() => {
-          cy.contains("Clinical research for Covid-19");
-        });
+        getTableau()
+          .should("be.visible")
+          .within(() => {
+            cy.contains("Clinical research for Covid-19");
+          });
 
         // Should have a feeback section
         getFeedback()
@@ -69,19 +80,21 @@ describe("Analysis page", () => {
           );
 
         // Should have footer section
-        getFooter().within(() => {
-          cy.server();
-          cy.route("POST", /hooks\.zapier\.com/, { status: "success" });
+        getFooter()
+          .should("be.visible")
+          .within(() => {
+            cy.server();
+            cy.route("POST", /hooks\.zapier\.com/, { status: "success" });
 
-          cy.contains("Stay updated with new analysis");
-          cy.get("input").type("hello@mail.co");
-          cy.get("button").click();
-          cy.contains("You have been successfully subscribed");
+            cy.contains("Stay updated with new analysis");
+            cy.get("input").type("hello@mail.co");
+            cy.get("button").click();
+            cy.contains("You have been successfully subscribed");
 
-          cy.contains("Want to go further?");
-          cy.contains("searching trials").click();
-          cy.location("pathname").should("eq", "/search");
-        });
+            cy.contains("Want to go further?");
+            cy.contains("searching trials").click();
+            cy.location("pathname").should("eq", "/search");
+          });
       });
     });
   });
