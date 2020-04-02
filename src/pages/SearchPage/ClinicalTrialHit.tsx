a3c674b43057d51b490e17d4945b2ba5c9938634
Yannick Tian <yannick.tian@gmail.com>
2020-04-02 12:14:20 +0200

    🎨 Move component at the end of the file

--- src/pages/SearchPage/ClinicalTrialHit.tsx
+++ src/pages/SearchPage/ClinicalTrialHit.tsx
@@ -34,6 +34,121 @@ interface ClinicalTrialHit {
   };
 }
 
+const Container = styled.div<{ hasPublications: boolean }>`
+  color: ${colors.GreySecondaryText};
+  background-color: ${colors.SecondaryBackground};
+  border: 1px solid ${colors.Border};
+  border-radius: 4px;
+  padding: 16px;
+
+  display: flex;
+  flex-direction: column;
+  ${({ hasPublications }) =>
+    hasPublications &&
+    css`
+      min-height: 144px;
+      justify-content: space-between;
+      ${LeftContainer} {
+        margin-bottom: 16px;
+      }
+      border-left: 4px solid ${colors.Primary};
+    `}
+`;
+
+const TopContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+
+  @media ${devices.Desktop} {
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-start;
+  }
+`;
+
+const LeftContainer = styled.div`
+  margin-bottom: 16px;
+  width: 100%; /* For IE11 */
+  @media ${devices.Desktop} {
+    width: 600px;
+    min-width: 600px;
+    margin-right: 64px;
+    margin-bottom: 0;
+  }
+`;
+
+const TitleContainer = styled.div`
+  font-size: 16px;
+  font-weight: 500;
+`;
+
+const Acronym = styled.span`
+  color: ${colors.GreySecondaryText};
+  white-space: nowrap;
+`;
+
+const StyledNavigationOutIcon = styled(NavigationOutIcon)`
+  visibility: hidden;
+  margin-left: 6px;
+  vertical-align: text-top;
+`;
+
+const Link = styled.a`
+  text-decoration: none;
+  color: ${colors.Primary};
+  &:hover {
+    color: ${colors.PrimaryHover};
+    ${Acronym} {
+      color: ${colors.PrimaryHover};
+    }
+    ${StyledNavigationOutIcon} {
+      visibility: visible;
+    }
+  }
+`;
+
+const RegistrationAndOutcomeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  > div {
+    margin-top: 5px;
+  }
+
+  @media ${devices.Desktop} {
+    flex-direction: row;
+    margin-top: 2px;
+    > div {
+      margin-top: 0;
+    }
+  }
+`;
+
+const RightContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  > div:not(:last-child) {
+    margin-top: 4px;
+  }
+  @media ${devices.Desktop} {
+    > div:not(:last-child):not(:first-child) {
+      margin-top: 4px;
+    }
+  }
+`;
+
+const PublicationsContainer = styled.div`
+  margin-top: 16px;
+  @media ${devices.Desktop} {
+    margin-top: 0;
+  }
+`;
+
+const PublicationsTitle = styled.div`
+  font-size: 12px;
+`;
+
 export const ClinicalTrialHit = ({
   hit: {
     public_title,
@@ -91,118 +206,3 @@ export const ClinicalTrialHit = ({
     )}
   </Container>
 );
-
-const PublicationsTitle = styled.div`
-  font-size: 12px;
-`;
-
-const Container = styled.div<{ hasPublications: boolean }>`
-  color: ${colors.GreySecondaryText};
-  background-color: ${colors.SecondaryBackground};
-  border: 1px solid ${colors.Border};
-  border-radius: 4px;
-  padding: 16px;
-
-  display: flex;
-  flex-direction: column;
-  ${({ hasPublications }) =>
-    hasPublications &&
-    css`
-      min-height: 144px;
-      justify-content: space-between;
-      ${LeftContainer} {
-        margin-bottom: 16px;
-      }
-      border-left: 4px solid ${colors.Primary};
-    `}
-`;
-
-const TopContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: center;
-
-  @media ${devices.Desktop} {
-    flex-direction: row;
-    align-items: center;
-    justify-content: flex-start;
-  }
-`;
-
-const RightContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  > div:not(:last-child) {
-    margin-top: 4px;
-  }
-  @media ${devices.Desktop} {
-    > div:not(:last-child):not(:first-child) {
-      margin-top: 4px;
-    }
-  }
-`;
-
-const TitleContainer = styled.div`
-  font-size: 16px;
-  font-weight: 500;
-`;
-
-const Acronym = styled.span`
-  color: ${colors.GreySecondaryText};
-  white-space: nowrap;
-`;
-
-const RegistrationAndOutcomeContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  > div {
-    margin-top: 5px;
-  }
-
-  @media ${devices.Desktop} {
-    flex-direction: row;
-    margin-top: 2px;
-    > div {
-      margin-top: 0;
-    }
-  }
-`;
-
-const LeftContainer = styled.div`
-  margin-bottom: 16px;
-  width: 100%; /* For IE11 */
-  @media ${devices.Desktop} {
-    width: 600px;
-    min-width: 600px;
-    margin-right: 64px;
-    margin-bottom: 0;
-  }
-`;
-
-const StyledNavigationOutIcon = styled(NavigationOutIcon)`
-  visibility: hidden;
-  margin-left: 6px;
-  vertical-align: text-top;
-`;
-
-const Link = styled.a`
-  text-decoration: none;
-  color: ${colors.Primary};
-  &:hover {
-    color: ${colors.PrimaryHover};
-    ${Acronym} {
-      color: ${colors.PrimaryHover};
-    }
-    ${StyledNavigationOutIcon} {
-      visibility: visible;
-    }
-  }
-`;
-
-const PublicationsContainer = styled.div`
-  margin-top: 16px;
-  @media ${devices.Desktop} {
-    margin-top: 0;
-  }
-`;
