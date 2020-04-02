cfec2f4828338891af8d38c607b75b74492a304d
Julien Fouilhé <julien.fouilhe@gmail.com>
2020-04-02 15:23:39 +0200

    Should find all results

--- functions/src/infrastructure/algolia/deserialize.test.ts
+++ functions/src/infrastructure/algolia/deserialize.test.ts
@@ -1,11 +1,11 @@
-import { deserializeSearchTrialsResponse } from "./deserialize";
+import * as Either from "fp-ts/lib/Either";
+import { deserializeSearchTrialsHits } from "./deserialize";
 
-describe("deserializeSearchTrialsResponse", () => {
+describe("deserializeSearchTrialsHits", () => {
   it("should deserialize a response with multiple hits", () => {
     const responseHits = [{ objectID: "objectID1" }, { objectID: "objectID2" }];
-    expect(deserializeSearchTrialsResponse(responseHits)).toStrictEqual([
-      "objectID1",
-      "objectID2"
-    ]);
+    expect(deserializeSearchTrialsHits(responseHits)).toStrictEqual(
+      Either.right(["objectID1", "objectID2"])
+    );
   });
 });
--- functions/src/infrastructure/algolia/deserialize.ts
+++ functions/src/infrastructure/algolia/deserialize.ts
@@ -1,11 +1,20 @@
 import * as decod from "decod";
+import * as Either from "fp-ts/lib/Either";
 import { toTrialId } from "../../domain";
+import { invalidInformationError } from "../../domain/errors";
 
 export const decodeTrialId = (value: unknown) => toTrialId(decod.string(value));
 
-export const deserializeSearchTrialsResponse = (
-  response: ReadonlyArray<unknown>
-) => response.map(hit => deserializeSearchTrial(hit));
+export const deserializeSearchTrialsHits = (hits: ReadonlyArray<unknown>) =>
+  Either.tryCatch(
+    () => hits.map(hit => deserializeSearchTrial(hit)),
+    e =>
+      invalidInformationError(
+        e instanceof Error
+          ? e.message
+          : "Unknown deserializing search trials hits error"
+      )
+  );
 
 const deserializeSearchTrial = (hit: unknown) =>
   decod.at("objectID", decodeTrialId)(hit);
