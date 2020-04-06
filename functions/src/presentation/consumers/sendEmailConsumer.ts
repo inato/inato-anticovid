c478b10b433a20c2f231e28e6a32f0d0aa8b4b63
Julien Fouilhé <julien.fouilhe@gmail.com>
2020-04-06 16:47:52 +0200

    Add logs

--- functions/src/presentation/consumers/sendEmailConsumer.ts
+++ functions/src/presentation/consumers/sendEmailConsumer.ts
@@ -2,6 +2,7 @@ import * as functions from "firebase-functions";
 import * as decod from "decod";
 import * as Either from "fp-ts/lib/Either";
 import * as TaskEither from "fp-ts/lib/TaskEither";
+import * as Task from "fp-ts/lib/Task";
 import { SubscriptionRepository, toSubscriptionId } from "../../domain";
 import { pipe } from "fp-ts/lib/pipeable";
 import { invalidInformationError } from "../../domain/errors";
@@ -11,31 +12,46 @@ import {
   IndexingService,
   EmailService,
   LoggingService,
-  timeServiceFactory
+  TimeService
 } from "../../application";
 
 export const sendEmailConsumer = ({
   subscriptionRepository,
   indexingService,
   emailService,
-  loggingService
+  loggingService,
+  timeService
 }: {
   subscriptionRepository: SubscriptionRepository;
   indexingService: IndexingService;
   emailService: EmailService;
   loggingService: LoggingService;
+  timeService: TimeService;
 }) => (message: functions.pubsub.Message, _context: functions.EventContext) =>
   pipe(
     TaskEither.fromEither(deserializeMessage(message.toJSON())),
-    taskEitherExtend(({ subscriptionId }) =>
-      sendEmail({
+    taskEitherExtend(({ subscriptionId }) => {
+      loggingService.log(
+        `Handling send email message for subscription ${subscriptionId}`
+      );
+      return sendEmail({
         subscriptionId,
         subscriptionRepository,
         indexingService,
         emailService,
         loggingService,
-        timeService: timeServiceFactory()
-      })
+        timeService
+      });
+    }),
+    TaskEither.fold(
+      e => {
+        loggingService.log("Error when sending email", e);
+        return Task.of(undefined);
+      },
+      () => {
+        loggingService.log("Sent email successfully");
+        return Task.of(undefined);
+      }
     )
   )();
 
