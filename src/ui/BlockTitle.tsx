ad55c4a8e614e26f68b9026d972120128d57f43e
vinassefranche <vincent.francois@inato.com>
2020-04-06 17:15:02 +0200

    Put the block title in the ui folder

---
+++ src/ui/BlockTitle.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+import { colors, fontSize, fontWeight, lineHeight, devices } from ".";
+
+export const BlockTitle = styled.h1`
+  font-family: Lora, Georgia;
+  color: ${colors.Primary};
+  font-size: ${fontSize.Medium};
+  font-weight: ${fontWeight.SemiBold};
+  line-height: ${lineHeight.Big};
+  margin: 0;
+
+  @media ${devices.Desktop} {
+    font-size: ${fontSize.Big};
+  }
+`;
