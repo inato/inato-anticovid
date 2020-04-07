ef0ad7241e4ed92b545248281af225d5f64ac283
Julien Fouilhé <julien.fouilhe@gmail.com>
2020-04-08 01:21:14 +0200

    Add criteria list

--- src/ui/texts.ts
+++ src/ui/texts.ts
@@ -7,12 +7,14 @@ export enum fontWeight {
 }
 
 export enum fontSize {
+  Small = "12px",
   Default = "14px",
   Medium = "24px",
   Big = "36px"
 }
 
 export enum lineHeight {
+  Small = "20px",
   Default = "24px",
   Big = "48px"
 }
