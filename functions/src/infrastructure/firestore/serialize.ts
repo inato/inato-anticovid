cef2e07b78344f3d5555b8ff68a86cd03d57ce16
Yannick Tian <yannick.tian@gmail.com>
2020-04-07 18:34:01 +0200

    🐛 Make hasResultsPublications optional

--- functions/src/infrastructure/firestore/serialize.ts
+++ functions/src/infrastructure/firestore/serialize.ts
@@ -1,6 +1,26 @@
 import * as Option from "fp-ts/lib/Option";
 import { Subscription, Search } from "../../domain";
 
+interface SerializedSearch {
+  search_query: string;
+  recruitment_status: string[];
+  therapeutic_classes: string[];
+  clinical_outcomes_extracted: string[];
+  surrogate_outcomes_extracted: string[];
+  study_types: string[];
+  countries: string[];
+  has_results_publications?: boolean;
+}
+
+const cleanUndefinedValues = <T>(obj: T): Partial<T> =>
+  Object.entries(obj).reduce((acc, [key, value]) => {
+    if (value === undefined) {
+      return acc;
+    }
+
+    return { ...acc, [key]: value };
+  }, {});
+
 export const serializeSearch = ({ searchQuery, facetFilters }: Search) => ({
   search_query: Option.getOrElse<string>(() => "")(searchQuery),
   recruitment_status: facetFilters.recruitmentStatus,
@@ -9,12 +29,12 @@ export const serializeSearch = ({ searchQuery, facetFilters }: Search) => ({
   surrogate_outcomes_extracted: facetFilters.surrogateOutcomesExtracted,
   study_types: facetFilters.studyTypes,
   countries: facetFilters.countries,
-  has_results_publications: facetFilters.hasResultsPublications
+  has_results_publications: facetFilters.hasResultsPublications,
 });
 
 export const serialize = (subscription: Subscription) => ({
   email: subscription.email.toString(),
   last_email_sent_date: subscription.lastEmailSentDate,
   search_results: subscription.searchResults.map(trialId => trialId.toString()),
-  search: serializeSearch(subscription.search)
+  search: cleanUndefinedValues<SerializedSearch>(serializeSearch(subscription.search))
 });
