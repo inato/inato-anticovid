d82152caa875315bf82d71af48371f75068cabdf
Julien Fouilhé <julien.fouilhe@gmail.com>
2020-04-08 12:04:13 +0200

    Fix modal on mobile

--- src/ui/theme/shadow.tsx
+++ src/ui/theme/shadow.tsx
@@ -1,12 +1,7 @@
-import { transparentize } from "polished";
-
 import { colors } from "../colors";
 
 import { spacings } from "./spacing";
 
 export const shadows = {
-  modal: `0 ${spacings.xs}px ${spacings.l}px ${transparentize(
-    0.9,
-    colors.ModalShadow
-  )}`
+  modal: `0 ${spacings.xs}px ${spacings.l}px ${colors.ModalShadow}`
 };
