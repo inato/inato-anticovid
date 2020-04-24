bd4d6c6fa9b185e3e9bb0a0fc662fe4c8379288c
Julien Fouilhé <julien.fouilhe@gmail.com>
2020-04-24 11:49:45 +0200

    Fix cypress test with invalid expectation

--- cypress/integration/search.spec.ts
+++ cypress/integration/search.spec.ts
@@ -6,15 +6,15 @@ import {
   getFilters,
   getCloseFiltersBtn,
   getOpenFiltersBtn,
-  getSearchContainer
-} from "./utils";
+  getSearchContainer,
+} from './utils';
 
-describe("Search page", () => {
+describe('Search page', () => {
   devices.forEach(deviceType => {
     describe(deviceType, () => {
       beforeEach(() => {
         // prevent window.fetch, fallback on xhr for cypress compat
-        cy.on("window:before:load", win => {
+        cy.on('window:before:load', win => {
           delete win.fetch;
         });
 
@@ -22,213 +22,211 @@ describe("Search page", () => {
         cy.device(deviceType);
 
         // go to search page
-        cy.visit("/search");
+        cy.visit('/search');
       });
 
-      it("smoke test search page", () => {
+      it('smoke test search page', () => {
         // should have a header
-        getHeader().should("be.visible");
+        getHeader().should('be.visible');
 
         // should have a filters panel only visible by default on large devices
         getFilters()
-          .shouldBeVisibleWhen(deviceType === "large")
+          .shouldBeVisibleWhen(deviceType === 'large')
           .within(() => {
             // the close filters button should not be visible
-            getCloseFiltersBtn().should("not.be.visible");
+            getCloseFiltersBtn().should('not.be.visible');
 
             // should have a reset filters button
-            cy.contains("button", "reset filters");
+            cy.contains('button', 'reset filters');
 
             // should have major sections
-            cy.contains("Only with published results");
-            cy.contains("Recruitment Status");
-            cy.contains("Therapeutic Classes")
+            cy.contains('Only with published results');
+            cy.contains('Recruitment Status');
+            cy.contains('Therapeutic Classes')
               .parent()
               // within the therapeutic classes section
               .within(() => {
                 // there should be an algolia search input
-                cy.get(".ais-SearchBox-input");
+                cy.get('.ais-SearchBox-input');
                 // and a show more button
-                cy.contains("button", "Show more");
+                cy.contains('button', 'Show more');
               });
-            cy.contains("Registration date")
+            cy.contains('Registration date')
               .parent()
               // within the registration date section
               .within(() => {
                 // there should be a slider
-                cy.get(".rheostat");
+                cy.get('.rheostat');
               });
-            cy.contains("Surrogate Outcome");
-            cy.contains("Study Type");
-            cy.contains("Countries")
+            cy.contains('Surrogate Outcome');
+            cy.contains('Study Type');
+            cy.contains('Countries')
               .parent()
               // within the countries section
               .within(() => {
                 // there should be an algolia search input
-                cy.get(".ais-SearchBox-input");
+                cy.get('.ais-SearchBox-input');
                 // and a show more button
-                cy.contains("button", "Show more");
+                cy.contains('button', 'Show more');
               });
           });
 
         // should have a open filters button not visible on large devices
-        getOpenFiltersBtn().shouldBeVisibleWhen(deviceType !== "large");
+        getOpenFiltersBtn().shouldBeVisibleWhen(deviceType !== 'large');
 
         // should have a search container
         getSearchContainer()
-          .should("be.visible")
+          .should('be.visible')
           .within(() => {
             // should have a search box
-            cy.get("input").should(
-              "have.attr",
-              "placeholder",
-              "Search by keyword, drug, NCTID, ..."
+            cy.get('input').should(
+              'have.attr',
+              'placeholder',
+              'Search by keyword, drug, NCTID, ...',
             );
 
             // should have suggestions
-            cy.contains("or try our suggestions")
+            cy.contains('or try our suggestions')
               .parent()
               .within(() => {
-                cy.get("a").should("have.length.greaterThan", 0);
+                cy.get('a').should('have.length.greaterThan', 0);
               });
 
             // should show number of trials found
             cy.contains(/\d+ trials found/)
-              .invoke("text")
-              .as("TotalNumberOfTrials");
+              .invoke('text')
+              .as('TotalNumberOfTrials');
 
             // should show first 20 trial cards
-            cy.get(".ais-Hits-item").should("have.length", 20);
+            cy.get('.ais-Hits-item').should('have.length', 20);
 
             // in the first card
-            cy.get(".ais-Hits-item:first")
+            cy.get('.ais-Hits-item:first')
               .children()
               .within(() => {
-                // there is a trial reference
-                cy.contains(/NCT\d+/).should("be.visible");
                 // a registration date
-                cy.contains("Registered on").should("be.visible");
+                cy.contains('Registered on').should('be.visible');
               });
           });
 
         // use search bar
         getSearchContainer().within(() => {
           // search for a prefix NCT04321
-          cy.get("input").type("NCT04321");
+          cy.get('input').type('NCT04321');
           // multiple trials found
           cy.contains(/\d+ trials found/)
-            .invoke("text")
-            .shouldNotEqAlias("@TotalNumberOfTrials");
+            .invoke('text')
+            .shouldNotEqAlias('@TotalNumberOfTrials');
           // type 993, text is now NCT04321993
-          cy.get("input").type("993");
+          cy.get('input').type('993');
           // exactly 1 trial found
-          cy.contains("1 trial found");
+          cy.contains('1 trial found');
           // type abc, text is now NCT04321993abc
-          cy.get("input").type("abc");
+          cy.get('input').type('abc');
           // no trials found
-          cy.contains("0 trial found");
+          cy.contains('0 trial found');
           // empty result shows up
-          cy.contains("No trial was found with these criteria");
-          cy.contains("Try to broaden your search or remove some filters");
+          cy.contains('No trial was found with these criteria');
+          cy.contains('Try to broaden your search or remove some filters');
           // clear search results
-          cy.get("button[type=reset]").click();
-          cy.get("input").should("have.text", "");
+          cy.get('button[type=reset]').click();
+          cy.get('input').should('have.text', '');
           cy.contains(/\d+ trials found/)
-            .invoke("text")
-            .shouldEqAlias("@TotalNumberOfTrials");
+            .invoke('text')
+            .shouldEqAlias('@TotalNumberOfTrials');
         });
 
         // open filters
-        if (deviceType !== "large") {
+        if (deviceType !== 'large') {
           getOpenFiltersBtn().click();
         }
 
         // use filters
         getFilters()
           // now visible on all devices
-          .should("be.visible")
+          .should('be.visible')
           // toggle the published results flag
-          .contains("Only with published results")
+          .contains('Only with published results')
           .parent()
           .within(() => {
             // capture the count of published results
-            cy.get(".ais-ToggleRefinement-count")
-              .invoke("text")
-              .as("OnlyPublishedCount");
+            cy.get('.ais-ToggleRefinement-count')
+              .invoke('text')
+              .as('OnlyPublishedCount');
             // and click on the checkbox
-            cy.get(".ais-ToggleRefinement-checkbox").click();
+            cy.get('.ais-ToggleRefinement-checkbox').click();
           });
 
         // close filters
-        if (deviceType !== "large") {
+        if (deviceType !== 'large') {
           getCloseFiltersBtn().click();
-          getFilters().should("not.be.visible");
+          getFilters().should('not.be.visible');
         }
 
         // check that the results are filtered
         getSearchContainer().within(() => {
-          cy.get<string>("@OnlyPublishedCount").then(count => {
+          cy.get<string>('@OnlyPublishedCount').then(count => {
             cy.contains(`${count} trials found`);
-            cy.get(".ais-Hits-item").should(
-              "have.length.greaterThan",
+            cy.get('.ais-Hits-item').should(
+              'have.length.greaterThan',
               // cypress does not have a "have.length.greaterThanOrEqual"
-              parseInt(count) - 1
+              parseInt(count) - 1,
             );
           });
         });
 
         // open filters
-        if (deviceType !== "large") {
+        if (deviceType !== 'large') {
           getOpenFiltersBtn().click();
         }
 
         // reset filters
         getFilters()
-          .contains("button", "reset filters")
+          .contains('button', 'reset filters')
           .click();
         getSearchContainer()
           .contains(/\d+ trials found/)
-          .invoke("text")
-          .shouldEqAlias("@TotalNumberOfTrials");
+          .invoke('text')
+          .shouldEqAlias('@TotalNumberOfTrials');
 
         // close filters
-        if (deviceType !== "large") {
+        if (deviceType !== 'large') {
           getCloseFiltersBtn().click();
-          getFilters().should("not.be.visible");
+          getFilters().should('not.be.visible');
         }
 
         // use first suggestion
         getSearchContainer().within(() => {
           cy.server();
-          cy.route("POST", /\/queries/).as("AlgoliaQuery");
-          cy.contains("or try our suggestions")
+          cy.route('POST', /\/queries/).as('AlgoliaQuery');
+          cy.contains('or try our suggestions')
             .parent()
-            .get("a:first")
+            .get('a:first')
             .click();
-          cy.wait("@AlgoliaQuery");
+          cy.wait('@AlgoliaQuery');
           cy.contains(/\d+ trials found/)
-            .invoke("text")
-            .shouldNotEqAlias("@TotalNumberOfTrials");
+            .invoke('text')
+            .shouldNotEqAlias('@TotalNumberOfTrials');
         });
 
         // open filters
-        if (deviceType !== "large") {
+        if (deviceType !== 'large') {
           getOpenFiltersBtn().click();
         }
 
         // reset filters
         getFilters()
-          .contains("button", "reset filters")
+          .contains('button', 'reset filters')
           .click();
         getSearchContainer()
           .contains(/\d+ trials found/)
-          .invoke("text")
-          .shouldEqAlias("@TotalNumberOfTrials");
+          .invoke('text')
+          .shouldEqAlias('@TotalNumberOfTrials');
 
         // close filters
-        if (deviceType !== "large") {
+        if (deviceType !== 'large') {
           getCloseFiltersBtn().click();
-          getFilters().should("not.be.visible");
+          getFilters().should('not.be.visible');
         }
       });
     });
