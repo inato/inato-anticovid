ff6d0764734c3d3219b06ea5ac767eecc2bb8cf8
Julien Fouilhé <julien.fouilhe@gmail.com>
2020-04-27 10:10:34 +0200

    Consent to cookies in cypress tests

--- cypress/integration/analysis.spec.ts
+++ cypress/integration/analysis.spec.ts
@@ -6,87 +6,89 @@ import {
   getFooter,
   getHeader,
   getIntro,
-  getTableau
-} from "./utils";
+  getTableau,
+} from './utils';
 
-describe("Analysis page", () => {
+describe('Analysis page', () => {
   devices.forEach(deviceType => {
     describe(deviceType, () => {
       beforeEach(() => {
         // prevent window.fetch, fallback on xhr for cypress compat
-        cy.on("window:before:load", win => {
+        cy.on('window:before:load', win => {
           delete win.fetch;
         });
 
         // select the device
         cy.device(deviceType);
 
+        cy.setCookie('cookie_consent', 'true');
+
         // go to root page
-        cy.visit("/");
+        cy.visit('/');
       });
 
-      it("smoke test analysis page", () => {
+      it('smoke test analysis page', () => {
         // should redirect to analysis page
-        cy.location("pathname").should("eq", "/analysis");
+        cy.location('pathname').should('eq', '/analysis');
 
         // should have a header
         getHeader()
-          .should("be.visible")
+          .should('be.visible')
           .within(() => {
             // should have the logo top left
             cy.get(`[alt="Inato Anti-Covid Logo"]`);
 
             // Should have links to analysis and search trial pages
-            cy.contains("a", "Analysis").should("have.class", "active");
-            cy.contains("a", "Search trials");
+            cy.contains('a', 'Analysis').should('have.class', 'active');
+            cy.contains('a', 'Search trials');
 
             // Should have send us feedback link
-            cy.contains("a", "Send us feedback").shouldBeVisibleWhen(
-              deviceType === "large"
+            cy.contains('a', 'Send us feedback').shouldBeVisibleWhen(
+              deviceType === 'large',
             );
           });
 
         // Should have intro section
         getIntro()
-          .should("be.visible")
+          .should('be.visible')
           .within(() => {
-            cy.contains("What is anticovid?");
-            cy.contains("Where the data comes from").click();
-            cy.contains("Most of the data comes");
-            cy.contains("Who we are").click();
-            cy.contains("Anticovid is provided by Inato");
+            cy.contains('What is anticovid?');
+            cy.contains('Where the data comes from').click();
+            cy.contains('Most of the data comes');
+            cy.contains('Who we are').click();
+            cy.contains('Anticovid is provided by Inato');
           });
 
         // Should have tableau section
         getTableau()
-          .should("be.visible")
+          .should('be.visible')
           .within(() => {
-            cy.contains("Clinical research for Covid-19");
+            cy.contains('Clinical research for Covid-19');
           });
 
         // Should have a feedback section
         getFeedback()
-          .shouldBeVisibleWhen(deviceType !== "large")
+          .shouldBeVisibleWhen(deviceType !== 'large')
           .within(() => {
-            cy.contains("Any questions or comments?");
-            cy.contains("a", "Send us feedback");
+            cy.contains('Any questions or comments?');
+            cy.contains('a', 'Send us feedback');
           });
 
         // Should have footer section
         getFooter()
-          .should("be.visible")
+          .should('be.visible')
           .within(() => {
             cy.server();
-            cy.route("POST", /hooks\.zapier\.com/, { status: "success" });
+            cy.route('POST', /hooks\.zapier\.com/, { status: 'success' });
 
-            cy.contains("Stay updated with new analysis");
-            cy.get("input").type("hello@mail.co");
-            cy.get("button").click();
-            cy.contains("You have been successfully subscribed");
+            cy.contains('Stay updated with new analysis');
+            cy.get('input').type('hello@mail.co');
+            cy.get('button').click();
+            cy.contains('You have been successfully subscribed');
 
-            cy.contains("Want to go further?");
-            cy.contains("searching trials").click();
-            cy.location("pathname").should("eq", "/search");
+            cy.contains('Want to go further?');
+            cy.contains('searching trials').click();
+            cy.location('pathname').should('eq', '/search');
           });
       });
     });
--- cypress/integration/methodology.spec.ts
+++ cypress/integration/methodology.spec.ts
@@ -1,90 +1,90 @@
 /// <reference path="../support/index.d.ts" />
 
 import {
-    devices,
-    getFeedback,
-    getFooter,
-    getHeader,
-    getMethodologyMain,
-  } from "./utils";
-  
-  describe("Methodology page", () => {
-    devices.forEach(deviceType => {
-      describe(deviceType, () => {
-        beforeEach(() => {
-          // prevent window.fetch, fallback on xhr for cypress compat
-          cy.on("window:before:load", win => {
-            delete win.fetch;
+  devices,
+  getFeedback,
+  getFooter,
+  getHeader,
+  getMethodologyMain,
+} from './utils';
+
+describe('Methodology page', () => {
+  devices.forEach(deviceType => {
+    describe(deviceType, () => {
+      beforeEach(() => {
+        // prevent window.fetch, fallback on xhr for cypress compat
+        cy.on('window:before:load', win => {
+          delete win.fetch;
+        });
+
+        // select the device
+        cy.device(deviceType);
+
+        cy.setCookie('cookie_consent', 'true');
+
+        // go to root page
+        cy.visit('/methodology');
+      });
+
+      it('smoke test methodology page', () => {
+        // should have a header
+        getHeader()
+          .should('be.visible')
+          .within(() => {
+            // should have the logo top left
+            cy.get(`[alt="Inato Anti-Covid Logo"]`);
+
+            // Should have links to analysis and search trial pages
+            cy.contains('a', 'Analysis');
+            cy.contains('a', 'Search trials');
+            cy.contains('a', 'Methodology').should('have.class', 'active');
+
+            // Should have send us feedback link
+            cy.contains('a', 'Send us feedback').shouldBeVisibleWhen(
+              deviceType === 'large',
+            );
           });
-  
-          // select the device
-          cy.device(deviceType);
-  
-          // go to root page
-          cy.visit("/methodology");
-        });
-  
-        it("smoke test methodology page", () => {  
-          // should have a header
-          getHeader()
-            .should("be.visible")
-            .within(() => {
-              // should have the logo top left
-              cy.get(`[alt="Inato Anti-Covid Logo"]`);
-  
-              // Should have links to analysis and search trial pages
-              cy.contains("a", "Analysis");
-              cy.contains("a", "Search trials");
-              cy.contains("a", "Methodology").should("have.class", "active");
-  
-              // Should have send us feedback link
-              cy.contains("a", "Send us feedback").shouldBeVisibleWhen(
-                deviceType === "large"
-              );
-            });
-  
-          // Should have the main section
-          getMethodologyMain()
-            .should("be.visible")
-            .within(() => {
-              cy.contains("Methodology");
-              cy.contains("Where do we find trials?").click();
-              cy.contains("Trials listed in Anticovid");
-              cy.contains("How often is updated our dataset?").click();
-              cy.contains("Every day");
-              cy.contains("Where do we find publications of results?").click();
-              cy.contains("we are leveraging several aggregators");
-              cy.contains("What processing is done for each trial?").click();
-              cy.contains("First, already structured data");
-            });
-  
-  
-          // Should have a feedback section
-          getFeedback()
-            .shouldBeVisibleWhen(deviceType !== "large")
-            .within(() => {
-              cy.contains("Any questions or comments?");
-              cy.contains("a", "Send us feedback");
-            });
-  
-          // Should have footer section
-          getFooter()
-            .should("be.visible")
-            .within(() => {
-              cy.server();
-              cy.route("POST", /hooks\.zapier\.com/, { status: "success" });
-  
-              cy.contains("Stay updated with new analysis");
-              cy.get("input").type("hello@mail.co");
-              cy.get("button").click();
-              cy.contains("You have been successfully subscribed");
-  
-              cy.contains("Explore ongoing clinical trials");
-              cy.contains("searching trials").click();
-              cy.location("pathname").should("eq", "/search");
-            });
-        });
+
+        // Should have the main section
+        getMethodologyMain()
+          .should('be.visible')
+          .within(() => {
+            cy.contains('Methodology');
+            cy.contains('Where do we find trials?').click();
+            cy.contains('Trials listed in Anticovid');
+            cy.contains('How often is updated our dataset?').click();
+            cy.contains('Every day');
+            cy.contains('Where do we find publications of results?').click();
+            cy.contains('we are leveraging several aggregators');
+            cy.contains('What processing is done for each trial?').click();
+            cy.contains('First, already structured data');
+          });
+
+        // Should have a feedback section
+        getFeedback()
+          .shouldBeVisibleWhen(deviceType !== 'large')
+          .within(() => {
+            cy.contains('Any questions or comments?');
+            cy.contains('a', 'Send us feedback');
+          });
+
+        // Should have footer section
+        getFooter()
+          .should('be.visible')
+          .within(() => {
+            cy.server();
+            cy.route('POST', /hooks\.zapier\.com/, { status: 'success' });
+
+            cy.contains('Stay updated with new analysis');
+            cy.get('input').type('hello@mail.co');
+            cy.get('button').click();
+            cy.contains('You have been successfully subscribed');
+
+            cy.contains('Explore ongoing clinical trials');
+            cy.contains('searching trials').click();
+            cy.location('pathname').should('eq', '/search');
+          });
       });
     });
   });
-  
+});
